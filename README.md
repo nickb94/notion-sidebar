5d7d731aeb654ff793026dc4388008e3d6c19705
Lahchhsin <nishchhalbhardwaj94@gmail.com>
2020-02-11 21:07:27 +0530

    initial commit:rocket:

---
+++ README.md
@@ -0,0 +1,15 @@
+### About
+I liked the locking sidebar on Notion.so workspace page, so I tried an almost looking vanilla JS recreation. DOM manipulation, Eventlisteners are loud here.
+
+### Technologies Used
+- HTML5
+- JavaScript
+- CSS3
+
+### How to use
+
+- Clone the repository, or copy/paste from Git.
+- Open index.html on browser.
+
+##### Note
+This is not an exact sidebar construction but works as a proof of concept. 
---
+++ app.js
@@ -0,0 +1,140 @@
+
+document.getElementById("btn").addEventListener("mouseenter", () => {
+
+    const a = document.getElementById("box");
+    a.style.transform = "translateX(0px)";
+    a.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+    a.style.opacity = "1";
+
+})
+
+
+document.getElementById("subheader").addEventListener("mousemove", () => {
+
+    box.classList.remove("unlocked");
+    box.style.transform = "translateX(-192px)";
+    box.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+
+})
+
+
+document.getElementById("slideout").addEventListener("mouseenter", () => {
+
+    const a = document.getElementById("box");
+    a.style.transform = "translateX(0px)";
+    a.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+    a.style.opacity = "1";
+
+})
+
+
+document.getElementById("left-div").addEventListener("mouseover", () => {
+
+    const a = document.getElementById("box");
+    a.style.transform = "translateX(-192px)";
+    a.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+    a.style.opacity = "1";
+
+})
+
+
+document.getElementById("cover").addEventListener("mouseleave", () => {
+
+    const a = document.getElementById("box");
+    a.style.transform = "translateX(-210px)";
+    a.style.opacity = "0";
+    a.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+
+})
+
+
+document.getElementById("header").addEventListener("mouseover", () => {
+
+    const a = document.getElementById("box");
+    a.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+    a.style.opacity = "1";
+
+})
+
+
+
+// on big button click
+document.getElementById("slideout").addEventListener("click", () => {
+
+
+    const box = document.getElementById("box");
+    box.classList.add("locked");
+    box.style.setProperty("transform", "translateX(-192px)");
+    box.style.setProperty("transition", "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)");
+
+
+
+    // add animation sliding transitions
+    const b = document.getElementById("right-div");
+    b.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+    const c = document.getElementById("left-div");
+    c.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+
+    // add classes on both moving divs
+    document.getElementById("right-div").classList.add("slider");
+    document.getElementById("left-div").classList.add("slider");
+
+    // toggle classes on cover, box and btn
+    document.getElementById("cover").classList.add("widthzero");
+    document.getElementById("btn").classList.add("hide");
+
+    // vanish big button clicked
+    document.getElementById("slideout").style.display = "none";
+
+    // create new button and append 
+    const bt = document.createElement("button");
+    bt.innerHTML = "<<";
+    bt.id = "sliderbtn";
+    document.getElementById("paddedname").appendChild(bt);
+
+    // on small button click
+    document.getElementById("sliderbtn").addEventListener("click", () => {
+
+        box.classList.remove("locked");
+        box.classList.add("unlocked");
+        box.style.setProperty("transform", "translateX(0px)");
+
+        document.getElementById("slideout").addEventListener("mouseenter", () => {
+
+            box.classList.remove("unlocked");
+
+        })
+
+        document.getElementById("subheader").addEventListener("mousemove", () => {
+
+            box.classList.remove("unlocked");
+            box.style.transform = "translateX(-192px)";
+            box.style.opacity = "1";
+            box.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+
+        })
+
+        // adding transitions
+        const b = document.getElementById("right-div");
+        b.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+        const c = document.getElementById("left-div");
+        c.style.transition = "0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000)";
+
+        // removing classlist from both divs
+        document.getElementById("right-div").classList.remove("slider");
+        document.getElementById("left-div").classList.remove("slider");
+
+        // removing created button
+        const n = document.querySelector("#sliderbtn");
+        n.parentNode.removeChild(n);
+
+        //removing hide and displaying those sections again
+        document.getElementById("cover").classList.remove("widthzero");
+        document.getElementById("btn").classList.remove("hide");
+
+        // displaying the button again
+        document.getElementById("slideout").style.display = "block";
+    })
+})
+
+
